a2dc2bf4a35123b5a7c2fa433fa8d7c05f0668a6
Monk <tbsanders5@gmail.com>
2020-10-31 15:25:41 -0700

    working heroku

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const htmlRoutes = require("./routes/htmlRoute.js");
 const apiRoutes = require("./routes/apiRoute.js");
-// const Workout = require("./models/workout.js");
-mongoose.connection.on('connected', () => {
-    console.log("Mongoose is connected!");
-  });
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,15 +13,6 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/api/workouts", (req, res) => {
-//     Workout.find({})
-//       .then(dbWorkout => {
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });
-//   });
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
